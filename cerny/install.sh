3e99a855b4dd766f9c9116d53f9f2f1cef115043
Nathan Cerny <ncerny@gmail.com>
2018-04-29 16:45:27 -0500

    Mount ESP partition to /boot, and fix tar path.
    Signed-off-by: Nathan Cerny <ncerny@gmail.com>

--- cerny/install.sh
+++ cerny/install.sh
@@ -11,18 +11,20 @@ timedatectl set-ntp true
 parted -s /dev/$1 mklabel gpt
 parted -s /dev/$1 mkpart ESP fat32 1MiB 551MiB
 parted -s /dev/$1 set 1 esp on
+parted -s /dev/$1 set 1 boot on
 parted -s /dev/$1 mkpart primary linux-swap 551MiB 4.5GiB
 parted -s /dev/$1 mkpart primary ext3 4.5GiB 100%
-parted -s /dev/$1 set 3 boot on
 
 mkfs.fat /dev/${1}1
 mkfs.ext4 /dev/${1}3
 mkswap /dev/${1}2
 swapon /dev/${1}2
 mount /dev/${1}3 /mnt
+mkdir -p /mnt/boot
+mount /dev/${1}1 /mnt/boot
 
 pacstrap /mnt base
 genfstab -U /mnt >> /mnt/etc/fstab
 
 curl -SsL https://api.bintray.com/content/habitat/stable/linux/x86_64/hab-%24latest-x86_64-linux.tar.gz?bt_package=hab-x86_64-linux -o /tmp/hab.tgz
-tar -xvzf hab.tgz -C /mnt/usr/local/bin
+tar -xvzf /tmp/hab.tgz -C /mnt/usr/local/bin
