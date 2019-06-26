8adc8de29d38137f048b7806564e270f6b782114
Nathan Cerny <ncerny@gmail.com>
2019-06-26 16:09:55 -0500

    tweak config for effortless.
    Signed-off-by: Nathan Cerny <ncerny@gmail.com>

--- core/habitat/plan.ps1
+++ core/habitat/plan.ps1
@@ -1,7 +1,9 @@
-$pkg_name=(Get-Content ..\metadata.rb | Select-String -pattern "^name") -Replace "name .(\w).", '$1'
+$pkg_name="core"
 $pkg_origin="ncerny"
 $pkg_version=(Get-Content ..\metadata.rb | Select-String -pattern "^version") -Replace "[^0-9.]", ''
 $pkg_maintainer="Nathan Cerny <ncerny@gmail.com>"
+$pkg_license=("Apache-2.0")
 $pkg_upstream_url="http://chef.io"
 $pkg_scaffolding="echohack/scaffolding-chef"
 $scaffold_policy_name="Policyfile"
+$scaffold_policyfiles_path="$PLAN_CONTEXT\.."
--- core/habitat/plan.sh
+++ core/habitat/plan.sh
@@ -1,9 +1,9 @@
-pkg_name=$(awk '/^name/ { tmp=substr($2,2,length($2)-2); print tmp }' ../metadata.rb)
-pkg_origin=ncerny
+pkg_name="core"
+pkg_origin="ncerny"
 pkg_version=$(awk '/^version/ { tmp=substr($2,2,length($2)-2); print tmp }' ../metadata.rb)
 pkg_maintainer="Nathan Cerny <ncerny@gmail.com>"
 pkg_license=("Apache-2.0")
+pkg_upstream_url="http://chef.io"
 pkg_scaffolding="echohack/scaffolding-chef"
 scaffold_policy_name="Policyfile"
-pkg_description="My base and infrastructure cookbook for my environment."
-pkg_upstream_url="https://github.com/ncerny/cookbooks/"
+scaffold_policyfiles_path="$PLAN_CONTEXT/.."
